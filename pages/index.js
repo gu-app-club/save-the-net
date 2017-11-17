129a87b47790afb4547289448f429df1c11535fb
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-11-17 15:17:02 -0800

    Base app

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Head from "../components/head";
+import Nav from "../components/nav";
+
+export default () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
+  </div>
+);
